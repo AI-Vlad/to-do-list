252199dd7fc83524f98707d7fe67cefc7f557310
Vladimir Andre Rojas <vrojas@FAZENDA.MS>
2022-05-17 15:23:10 -0300

    testando correção de state

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -54,7 +54,7 @@ export default function Home(props) {
 
     } else {
 
-      setTarefas([...tarefas, tarefa])
+      setTarefas(tarefas? [...tarefas, tarefa] : [])
       setTarefa("")
       setTarefasSalvas(tarefas)
 
