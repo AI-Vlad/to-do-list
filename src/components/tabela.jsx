910435aa149be4fa5fc6688cd8ac6d8eff406ef7
Vladimir Andre Rojas <vrojas@FAZENDA.MS>
2022-05-17 13:53:25 -0300

    corrigindo erro de renderização por condição de existência do array no componente tabela

--- src/components/tabela.jsx
+++ src/components/tabela.jsx
@@ -1,26 +1,23 @@
-import {FaEdit, FaWindowClose} from 'react-icons/fa'
+import { FaEdit, FaWindowClose } from 'react-icons/fa'
 
 export default function Tabela(props) {
-
+  if (props.tarefas) {
     return (
-
       <ul className={props.className}>
-      {props.tarefas.map((item, index) => {
-        return <li key={index} >
-          <div>
-            {item}
-          </div>
-          <div>
-            <FaEdit onClick={() => props.handleEdit(index)} />
-            <FaWindowClose onClick={() => props.handleDelete(index)} />
-          </div>
-        </li>
-      }
-
-      )
-      }
-    </ul>
-
+        {props.tarefas.map((item, index) => {
+          return <li key={index} >
+            <div>
+              {item}
+            </div>
+            <div>
+              <FaEdit onClick={() => props.handleEdit(index)} />
+              <FaWindowClose onClick={() => props.handleDelete(index)} />
+            </div>
+          </li>
+        })}
+      </ul>
     )
+  }
 
+  return false
 }
